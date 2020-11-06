cdc78919af5655076188798f1eb6d332bbcee712
SoudehBabaeizadeh <babaeizadeh.s@gmail.com>
2020-11-06 19:57:36 +0330

    first version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "Reading Book" },
+      { id: 2, content: "Codding" },
+    ],
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({ todos })
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => todo.id !== id)
+    this.setState({ todos })
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1
+          className="center teal lighten-1"
+          style={{ color: "white", borderRadius: "5px" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Daily Notes
+        </h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/AddTodo.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react"
+
+class AddTodo extends Component {
+  state = {
+    content: "",
+  }
+
+  handleChange = (e) => {
+    this.setState({ content: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (!this.state.content) return
+    this.props.addTodo(this.state)
+    this.setState({ content: "" })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">Add Notes</label>
+          <input
+            type="text"
+            value={this.state.content}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AddTodo
